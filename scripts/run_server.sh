d1df5cf3dc94c3343d7892a610879d8789fefb84
Mike <34043825+Mlawrence95@users.noreply.github.com>
2026-01-26 01:07:26 -0800

    Update run_server.sh

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,27 +3,27 @@ source ~/terraria/terraria-server/scripts/shared_variables.sh
 
 set -ex
 
-run_terraria_server_with_flags() {
-  STATIC_FLAGS="-config ${SERVER_CONFIG_PATH}"
-  # Run the server binary with the static flags
-  "$SERVER_BINARY_PATH" $STATIC_FLAGS
-}
-
-
-# Function to create tmux session and start the server
 start_server_in_background() {
   # Check if a tmux session is already running
-  if tmux has-session -t $TMUX_SESSION_NAME 2>/dev/null; then
+  if tmux has-session -t "$TMUX_SESSION_NAME" 2>/dev/null; then
     echo "tmux session '$TMUX_SESSION_NAME' is already running!"
   else
     echo "Starting Terraria server in tmux session..."
-    tmux new-session -d -s "$TMUX_SESSION_NAME" "$(declare -f run_terraria_server_with_flags); run_terraria_server_with_flags"
+
+    # Start the tmux session with the server command
+    tmux new-session -d -s "$TMUX_SESSION_NAME" bash -c "
+      source ~/terraria/terraria-server/scripts/shared_variables.sh;
+      STATIC_FLAGS='-config ${SERVER_CONFIG_PATH}';
+      echo 'Starting Terraria server with flags: $STATIC_FLAGS';
+      \"$SERVER_BINARY_PATH\" $STATIC_FLAGS;
+      bash"  # Keep tmux session open for further commands after server exits
 
     echo "Server started in tmux session '$TMUX_SESSION_NAME'."
     echo "To detach from tmux, press <Ctrl+b> then d."
     echo "To reattach to the tmux session, run: tmux attach -t $TMUX_SESSION_NAME"
   fi
 }
+
 
 
 start_server_in_background
