50bf157faf45e38f01ff0b538317212102c6d751
Mike <34043825+Mlawrence95@users.noreply.github.com>
2026-02-01 09:09:08 -0800

    cd into terraria dir in constants file

--- scripts/shared_variables.sh
+++ scripts/shared_variables.sh
@@ -1,9 +1,10 @@
-export WORKSPACE_ROOT=$(realpath "${HOME}/terraria/terraria-server")
+export WORKSPACE_ROOT="${HOME}/terraria/terraria-server"
+cd "${WORKSPACE_ROOT}" || exit
 echo "Resolved workspace root path to ${WORKSPACE_ROOT}"
 
 # Binary info
 # Pick one from https://terraria.wiki.gg/wiki/Server#Server_files
-export SERVER_URL"https://terraria.org/api/download/pc-dedicated-server/terraria-server-1453.zip"
+export SERVER_URL="https://terraria.org/api/download/pc-dedicated-server/terraria-server-1453.zip"
 export VERSION_SHORTNAME=1453
 export EXE_ROOT="${WORKSPACE_ROOT}/${VERSION_SHORTNAME}/Linux/"
 export SERVER_BINARY_PATH="${EXE_ROOT}/TerrariaServer.bin.x86_64"
@@ -14,6 +15,7 @@ export WORLD_SAVES_PATH="${WORKSPACE_ROOT}/worlds"
 export WORLD_FILE_PATH="${WORLD_SAVES_PATH}/main_world_file.wld"
 export BACKUPS_PATH="${HOME}/backups/"
 export LOG_FILE_PATH="${WORKSPACE_ROOT}/server_logs.txt"
+export CRONTAB_CONFIG_PATH="${WORKSPACE_ROOT}/scripts/cronjobs"
 
 # Background process session management
 export SCREEN_SESSION_NAME=terraria-server
