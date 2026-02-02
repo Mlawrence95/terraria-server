a64eeca13e552c2e689f15bb10618d1c690126c0
Mike <34043825+Mlawrence95@users.noreply.github.com>
2026-02-01 18:57:28 -0800

    Ping server with water check every 30 mins

---
+++ scripts/water_reminder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load constants
+source "${HOME}/terraria/terraria-server/scripts/shared_variables.sh"
+source "${HOME}/terraria/terraria-server/scripts/server_admin.sh"
+
+send_global_message "Hydration check"
